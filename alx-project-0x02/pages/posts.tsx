b1935c09c3ffc9e59c5852bd06674ac4bb67b0cd
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-28 10:28:06 +0300

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/layout/Header'
 export default function PostsPage() {
   return (
     <>
+	<Header />
       <main className="p-4">
         <h1 className="text-2xl font-bold">Posts Page</h1>
       </main>
