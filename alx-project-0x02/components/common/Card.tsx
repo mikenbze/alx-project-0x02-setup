ce201bbcdfd90547b9c6743a571bbd56a4fd9d61
Michael Muriungi <50227674+mikenbze@users.noreply.github.com>
2025-07-28 09:43:40 +0300

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,12 +1,29 @@
 // components/common/Card.tsx
 
-import { CardProps } from '@/interfaces'
+//import { CardProps } from '@/interfaces'
 
-export default function Card({ title, content }: CardProps) {
+//export default function Card({ title, content }: CardProps) {
+ // return (
+  //  <div className="bg-white rounded-xl shadow-md p-6 w-full max-w-md mx-auto mb-6 border border-gray-200">
+  //    <h2 className="text-xl font-semibold mb-2 text-blue-700">{title}</h2>
+  //    <p className="text-gray-700">{content}</p>
+ //   </div>
+ // )
+//}
+
+
+
+// components/common/Card.tsx
+import React from 'react'
+import { type CardProps } from '@/interfaces'
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="bg-white rounded-xl shadow-md p-6 w-full max-w-md mx-auto mb-6 border border-gray-200">
-      <h2 className="text-xl font-semibold mb-2 text-blue-700">{title}</h2>
-      <p className="text-gray-700">{content}</p>
+    <div className="rounded-2xl shadow-md p-4 bg-white">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p>{content}</p>
     </div>
   )
 }
+
+export default Card
